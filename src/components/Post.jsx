bae5396a18c16e28c66975e4151c47e9048e0059
solux <lee.yeji0327@gmail.com>
2024-09-01 12:16:43 +0900

    hashtags array 분리

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import hashtags from "../hashtag/hashtag";
 
 const Box = styled.div`
   padding: 0px 15px;
@@ -10,35 +11,6 @@ const Box = styled.div`
 `;
 
 const Post = ({ name, content, selectedTagId }) => {
-  const hashtags = [
-    { id: 1, name: "A" },
-    { id: 2, name: "B" },
-    { id: 3, name: "C" },
-    { id: 4, name: "D" },
-    { id: 5, name: "E" },
-    { id: 6, name: "F" },
-    { id: 7, name: "G" },
-    { id: 8, name: "H" },
-    { id: 9, name: "I" },
-    { id: 10, name: "J" },
-    { id: 11, name: "K" },
-    { id: 12, name: "L" },
-    { id: 13, name: "M" },
-    { id: 14, name: "N" },
-    { id: 15, name: "O" },
-    { id: 16, name: "P" },
-    { id: 17, name: "Q" },
-    { id: 18, name: "R" },
-    { id: 19, name: "S" },
-    { id: 20, name: "T" },
-    { id: 21, name: "U" },
-    { id: 22, name: "V" },
-    { id: 23, name: "W" },
-    { id: 24, name: "X" },
-    { id: 25, name: "Y" },
-    { id: 26, name: "Z" },
-  ];
-
   const selectedTag = hashtags.find((tag) => tag.id === selectedTagId);
 
   // 태그가 선택되지 않았거나, 태그가 일치하는 경우에만 포스트를 렌더링
--- src/components/TagBar.jsx
+++ src/components/TagBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import "../style/TagBar.css";
 import HashTag from "./HashTag";
+import hashtags from "../hashtag/hashtag";
 
 const Bar = styled.div`
   background-color: #dedede;
@@ -9,35 +10,6 @@ const Bar = styled.div`
 
 const TagBar = ({ onTagSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const hashtags = [
-    { id: 1, name: "A" },
-    { id: 2, name: "B" },
-    { id: 3, name: "C" },
-    { id: 4, name: "D" },
-    { id: 5, name: "E" },
-    { id: 6, name: "F" },
-    { id: 7, name: "G" },
-    { id: 8, name: "H" },
-    { id: 9, name: "I" },
-    { id: 10, name: "J" },
-    { id: 11, name: "K" },
-    { id: 12, name: "L" },
-    { id: 13, name: "M" },
-    { id: 14, name: "N" },
-    { id: 15, name: "O" },
-    { id: 16, name: "P" },
-    { id: 17, name: "Q" },
-    { id: 18, name: "R" },
-    { id: 19, name: "S" },
-    { id: 20, name: "T" },
-    { id: 21, name: "U" },
-    { id: 22, name: "V" },
-    { id: 23, name: "W" },
-    { id: 24, name: "X" },
-    { id: 25, name: "Y" },
-    { id: 26, name: "Z" },
-  ];
 
   const toggleBar = () => {
     setIsOpen(!isOpen);
