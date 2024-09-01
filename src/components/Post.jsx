c7c2d058796ef3905fc6642641bd97e5770a0eb4
solux <lee.yeji0327@gmail.com>
2024-09-01 12:32:18 +0900

    코드 설명 추가

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,9 +11,11 @@ const Box = styled.div`
 `;
 
 const Post = ({ name, content, selectedTagId }) => {
+  // props로 내려받은 선택된 해시태그 id를 통해 hashtags 배열에서 해당 부분을 찾음
   const selectedTag = hashtags.find((tag) => tag.id === selectedTagId);
 
-  // 태그가 선택되지 않았거나, 태그가 일치하는 경우에만 포스트를 렌더링
+  // 선택된 태그 이름이 존재하지 않거나(아직 태그 선택 안 한 경우),
+  // prop으로 내려받은 posts의 name과 선택된 태그 이름이 일치하는 경우만 렌더링(선택한 태그만 렌더링)
   if (!selectedTag || selectedTag.name === name) {
     return (
       <Box>
--- src/components/SelectedTag.jsx
+++ src/components/SelectedTag.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import hashtags from "../hashtag/hashtag";
 
 const SelectedTag = ({ selectedTagId }) => {
+  // props로 내려받은 선택된 해시태그 id를 통해 hashtags 배열에서 해당 부분을 찾음
   const selectedTag = hashtags.find((tag) => tag.id === selectedTagId);
 
   return (
